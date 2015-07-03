74fb6b5f0f8a3c4b0e7a26ca2d5dcaf3f3ffce26
Adam Goucher <adam@goucher.ca>
2015-07-03 12:10:52 -0400

    first

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'saunter'
+maintainer       'Element 34'
+maintainer_email 'adam@goucher.ca'
+license          'Apache 2.0'
+description      'Installs/Configures saunter'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          'python'
---
+++ recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: saunter
+# Recipe:: default
+#
+# Copyright 2015, Element 34
+#
+# All rights reserved - Do Not Redistribute
+#
+
+python_pip "saunter"
